129fea47b8ec3cf6588a9f669964d827447f122c
Vinicius Dias <carlosv775@gmail.com>
2019-05-11 12:21:40 -0300

    Código finalizado
    Vídeo 2.3

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,8 +1,20 @@
 <?php
 
+require 'vendor/autoload.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
 $resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
