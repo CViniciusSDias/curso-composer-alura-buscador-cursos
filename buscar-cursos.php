15ce187e28b989f5ead4b62d02176cd931b9d43c
Vinicius Dias <carlosv775@gmail.com>
2019-05-11 15:56:41 -0300

    Adicionando arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -13,5 +14,5 @@ $buscardor = new Buscador($client, $crawler);
 $cursos = $buscardor->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
